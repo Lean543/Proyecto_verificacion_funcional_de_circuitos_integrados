4b8196eccf5a4190d9ac8c6f418ce5a40723e45b
Leandro <leandro543fj@gmail.com>
2026-05-09 09:38:41 -0600

    Simplicando top

--- testbench/top.sv
+++ testbench/top.sv
@@ -4,12 +4,16 @@
 
 module top();
 
-   	logic  XCLK;
-    logic  XRES;
+   	logic  clk;
+    logic  res;
+
+    ifc_riskv ifc_riskv_obj(clk);
+
+  	testcase test(ifc_riskv_obj);
 
     darksocv DUT (
-        .XCLK    (XCLK),
-        .XRES    (XRES)
+        .XCLK    (clk),
+        .XRES    (res)
     );
 
     //initial begin
@@ -17,15 +21,22 @@ module top();
     //   $dumpvars(0, testbench);
     //end
 
-    always #1 XCLK = ~XCLK;
+    always #1 clk = ~clk;
+
+    genvar i;
+    generate
+        for (i = 0; i < 16; i++) begin
+            assign ifc_riskv_obj.regs[i] = DUT.core0.REGS[i];
+        end
+    endgenerate
 
     initial begin
-        XCLK = 1'b1;
-      	XRES = 1'b0; // es activo en bajo
+        clk = 1'b1;
+      	res = 1'b0; // es activo en bajo
 
       #1000;
 
-        XRES = 1'b1;
+        res = 1'b1;
 
         #10000 $finish;
     end
@@ -47,26 +58,5 @@ module top();
     //     wire [31:0] x13 = DUT.core0.REGS[13];
     //     wire [31:0] x14 = DUT.core0.REGS[14];
     //     wire [31:0] x15 = DUT.core0.REGS[15];
-
-    assign ifc_riskv_obj.regs[0]  = DUT.core0.REGS[0];
-    assign ifc_riskv_obj.regs[1]  = DUT.core0.REGS[1];
-    assign ifc_riskv_obj.regs[2]  = DUT.core0.REGS[2];
-    assign ifc_riskv_obj.regs[3]  = DUT.core0.REGS[3];
-    assign ifc_riskv_obj.regs[4]  = DUT.core0.REGS[4];
-    assign ifc_riskv_obj.regs[5]  = DUT.core0.REGS[5];
-    assign ifc_riskv_obj.regs[6]  = DUT.core0.REGS[6];
-    assign ifc_riskv_obj.regs[7]  = DUT.core0.REGS[7];
-    assign ifc_riskv_obj.regs[8]  = DUT.core0.REGS[8];
-    assign ifc_riskv_obj.regs[9]  = DUT.core0.REGS[9];
-    assign ifc_riskv_obj.regs[10]  = DUT.core0.REGS[10];
-    assign ifc_riskv_obj.regs[11]  = DUT.core0.REGS[11];
-    assign ifc_riskv_obj.regs[12]  = DUT.core0.REGS[12];
-    assign ifc_riskv_obj.regs[13]  = DUT.core0.REGS[13];
-    assign ifc_riskv_obj.regs[14]  = DUT.core0.REGS[14];
-    assign ifc_riskv_obj.regs[15]  = DUT.core0.REGS[15];
-
-    ifc_riskv ifc_riskv_obj(XCLK);
-
-  	testcase test(ifc_riskv_obj);
 
 endmodule
